6f3798ecf86debf266964499faf3f0278da58082
Kevin DeMarco <kevin.demarco@gmail.com>
2021-07-30 00:11:14 -0400

    Working bamboomba in sim

--- bamboomba_gazebo/launch/cafe.launch.py
+++ bamboomba_gazebo/launch/cafe.launch.py
@@ -1,21 +1,4 @@
 #!/usr/bin/env python3
-#
-# Copyright 2019 ROBOTIS CO., LTD.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-# Authors: Darby Lim
-
 import os
 from pathlib import Path
 
@@ -27,18 +10,16 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration, Command, EnvironmentVariable, PathJoinSubstitution
 
 def generate_launch_description():
-    use_sim_time = LaunchConfiguration('use_sim_time', default='True')
+    use_sim_time = LaunchConfiguration('use_sim_time')
     world = os.path.join(get_package_share_directory('bamboomba_gazebo'),
                          'worlds', 'cafe.world')
 
     pkg_gazebo_ros = get_package_share_directory('gazebo_ros')
 
-    xacro_path = os.path.join(get_package_share_directory('bamboomba_description'),
-                              'urdf', 'bamboomba.urdf.xacro')
-    robot_description = {'robot_description' : Command(['xacro', ' ', xacro_path])}
-
-    rviz_config = os.path.join(get_package_share_directory('bamboomba_description'),
-                              'rviz', 'bamboomba.rviz')
+    robot_state_publisher = os.path.join(
+        get_package_share_directory('bamboomba_description'),
+        'launch',
+        'robot_state_publisher.launch.py')
 
     # We need to append to the GAZEBO_MODEL_PATH for gazebo to find the Bamboomba's meshes
     create_description_path = str(Path(get_package_share_directory('create_description')).parent.absolute())
@@ -46,7 +27,7 @@ def generate_launch_description():
     return LaunchDescription([
         DeclareLaunchArgument(
             'use_sim_time',
-            default_value='false',
+            default_value='True',
             description='Use simulation (Gazebo) clock if true'),
 
         SetEnvironmentVariable(name='GAZEBO_MODEL_PATH', value=[EnvironmentVariable('GAZEBO_MODEL_PATH'), os.pathsep, create_description_path]),
@@ -64,6 +45,11 @@ def generate_launch_description():
             ),
         ),
 
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(robot_state_publisher),
+            launch_arguments={'use_sim_time': use_sim_time}.items()
+        ),
+
         Node(package='gazebo_ros',
              executable='spawn_entity.py',
              arguments=['-entity', 'bamboomba',
@@ -71,12 +57,4 @@ def generate_launch_description():
                         '-package_to_model',
                         '-z 0.1'],
              output='screen'),
-
-        Node(package='robot_state_publisher',
-             name='robot_state_publisher',
-             executable='robot_state_publisher',
-             output='screen',
-             parameters=[robot_description,
-                         {'use_sim_time': use_sim_time}]
-        ),
     ])
